c2b863d321075345601bc1f0ae33ef16acd39647
Shawn <spalmer12121@gmail.com>
2021-07-21 16:16:19 -0400

    Added files for the CSC 120 course

---
+++ EvenOddTester.py
@@ -0,0 +1,10 @@
+number = input("Please enter a number: ")
+
+if int (number) % 2 == 0:
+     print ("even number")
+
+else:
+     print ("odd number")
+
+if int (number) < 0:
+     print ("negative number")
